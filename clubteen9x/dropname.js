f28be3115d298b599c22ff98a211d1073f63e060
MarioH8866@gmail.com <MarioH8866@gmail.com@c90cc93e-317c-aa0e-8cd9-1b3ea32f5173>
2012-03-08 03:24:44 +0000

    tyy

---
+++ clubteen9x/dropname.js
@@ -0,0 +1,53 @@
+﻿function hienmenu(a, b, c) {
+    
+    var d = document.getElementById(b);
+    var e = d.style.visibility;
+    if (e == 'hidden') {
+         
+        var f = FindXY(a);
+        d.style.visibility = 'visible';
+        overFlowX = f['x'] + d.offsetWidth - document.body.offsetWidth;
+        f['x'] = overFlowX > 0 ? f['x'] - overFlowX : f['x'];
+        d.style.left = (f['x'] - 30) + 'px';
+        d.style.top = (f['y'] - 105) + 'px';
+
+        $(function () {
+            $('#hienhinh a:eq(0)').attr('href', '/u' + c);
+            $('#hienhinh a:eq(1)').attr('href', '/u' + c + 'wall');
+            $('#hienhinh a:eq(2)').attr('href', '/u' + c + 'stats');
+            $('#hienhinh a:eq(3)').attr('href', '/u' + c + 'friends');
+            $('#hienhinh a:eq(4)').attr('href', '/privmsg?mode=post&u=' + c);
+            $('#hienhinh a:eq(5)').attr('href', '/postp/' + c)
+        });
+   
+    } else { 
+        var f = FindXY(a);
+        d.style.visibility = 'visible';
+        overFlowX = f['x'] + d.offsetWidth - document.body.offsetWidth;
+        f['x'] = overFlowX > 0 ? f['x'] - overFlowX : f['x'];
+        d.style.left = (f['x'] - 30) + 'px';
+        d.style.top = (f['y'] - 105) + 'px';
+
+        $(function () {
+            $('#hienhinh a:eq(0)').attr('href', '/u' + c);
+            $('#hienhinh a:eq(1)').attr('href', '/u' + c + 'wall');
+            $('#hienhinh a:eq(2)').attr('href', '/u' + c + 'stats');
+            $('#hienhinh a:eq(3)').attr('href', '/u' + c + 'friends');
+            $('#hienhinh a:eq(4)').attr('href', '/privmsg?mode=post&u=' + c);
+            $('#hienhinh a:eq(5)').attr('href', '/postp/' + c)
+        });
+     
+    }
+};
+function an() {
+    var b = document.getElementById('hienhinh');
+    b.style.visibility = 'hidden'
+};
+$(function () {
+   
+    $(".username a[href^='/u']").click(function () {
+        hienmenu(this, 'hienhinh', $(this).attr('href').slice(2));
+        return false
+    })
+});
+
