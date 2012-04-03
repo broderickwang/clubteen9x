51bb61d97209d467b912fdc5910163c97a6a3c44
MarioH8866@gmail.com <MarioH8866@gmail.com@c90cc93e-317c-aa0e-8cd9-1b3ea32f5173>
2012-04-03 06:10:38 +0000

    f

---
+++ trunk/thuan007/19608.js
@@ -0,0 +1,1 @@
+$(function(){$("#c3login").attr('action','/login?redirect='+this.location.pathname);$("#pwd_bad").html('Quá đơn giản <img src="/users/2315/72/47/45/smiles/391570.gif">');$("#pwd_middle").html('Chuẩn đấy <img src="/users/2315/72/47/45/smiles/434257.gif">');$("#pwd_good").html('Khỏi cần chỉnh <img src="/users/2315/72/47/45/smiles/762514.gif">')});
