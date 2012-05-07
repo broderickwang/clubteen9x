3f6b321555debb2fb449ac0f7c8cdaeb5f1adfe0
MarioH8866@gmail.com <MarioH8866@gmail.com@c90cc93e-317c-aa0e-8cd9-1b3ea32f5173>
2012-05-07 08:24:58 +0000

    777777

---
+++ trunk/9xstudio/phpbb2/load[1].js
@@ -0,0 +1,22 @@
+﻿//
+<!--
+Dim Bar, Line, SP
+Bar = 0
+Line = "|"
+SP = 100
+Function Window_onLoad()
+Bar = 95
+SP = 10
+End Function
+Function Count()
+If Bar < 100 Then
+Bar = Bar + 1
+Window.Status = "Website  Đang Tải Về  " & Bar & "%" & " " & String(Bar, Line)
+setTimeout "Count()", SP
+Else
+Window.Status = "|He Thong Am Nhac Tinh Ban Thuoc Website wWw.letrieuthien.com | Chuc Moi Nguoi Vui Ve |"
+Document.Body.Style.Display = ""
+End If
+End Function
+Call Count()
+-->
