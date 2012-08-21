f0113cd2cc8a387ee5e3b2ab9ba9cf625a2dd8d7
MarioH8866@gmail.com <MarioH8866@gmail.com@c90cc93e-317c-aa0e-8cd9-1b3ea32f5173>
2012-08-21 03:44:06 +0000

    u

---
+++ forumotion/phpbb3/17000.js
@@ -0,0 +1,1 @@
+$(window).load(function(){$('.blog_message img,.postbody img').show(function(){if($(this).width()>=200){$(this).before('<div style="position:relative;"><img style="z-index:10; position:absolute; left:-10px; top:-10px; opacity: 0.7;filter: alpha(opacity=70);" src="http://static10.ichodientuvn.com/pict/stores/thucongtruyenthong/assets/img/logo.gif"></img></div>')}});
